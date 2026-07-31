67553994c9786f0ca2e284385115e2827c3c006b
lokesh nagrikar <lokeshnagrikar14@gmail.com>
2026-07-31 09:15:39 +0530

    fix: Upgrade Next.js to 15.1.7 to resolve Vercel build security policy

--- components/providers/SmoothScrollProvider.tsx
+++ components/providers/SmoothScrollProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import { gsap, ScrollTrigger } from "../../lib/gsap";
 
 interface SmoothScrollProviderProps {
